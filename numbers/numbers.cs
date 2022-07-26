5981fe6057481a347444f1790903be3743569ce1
kksr1996 <67324147+kksr1996@users.noreply.github.com>
2022-07-26 15:55:59 +0530

    Adding proj

---
+++ numbers/numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers
+{
+    internal class numbers
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0) { Console.WriteLine("positive number"); }
+            else if (a == 0) { Console.WriteLine(" number is 0"); }
+            else { Console.WriteLine("number is negative"); }
+
+        }
+    }
+}
