31043d97a9e03d429958fc59921906635ca4181a
kksr1996 <67324147+kksr1996@users.noreply.github.com>
2022-07-26 15:54:26 +0530

    Adding programs

---
+++ Max_three_nos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Programming
+{
+    //2. Write a program to find maximum between three numbers.
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter first no!");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second no!");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second no!");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c) { Console.WriteLine("A is maximum!"); }
+            else if (b > a && b > c) { Console.WriteLine("B is maximum " + b); }
+            else { Console.WriteLine("C is maximum " + c); }
+        }
+    }
+}
